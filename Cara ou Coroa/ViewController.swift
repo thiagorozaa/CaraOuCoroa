6e3e20930e62d62c905081699fdb107298290f94
thiagorozaa <thiago.rozaa@gmail.com>
2020-11-02 08:00:49 -0300

    Initial Commit

---
+++ Cara ou Coroa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cara ou Coroa
+//
+//  Created by Thiago Rosa Ataide on 02/11/20.
+//  Copyright © 2020 Thiago Rosa Ataide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
